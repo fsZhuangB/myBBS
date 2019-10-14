e41aee49aab2becc8b1f974137f43caff2f142d9
fszhuangb <617670307@qq.com>
2019-10-14 20:57:00 +0800

    add blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
     使用程序工厂函数
     延迟创建程序实例，把创建过程移到可显式调用的工厂函数中
 """
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 # from flask_mail import Mail
@@ -17,8 +18,10 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
+    from .main import main as main_blueprint
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    app.register_blueprint(main_blueprint) # 注册蓝本
     config[config_name].init_app(app)
     bootstrap.init_app(app)
     # mail.init_app(app)
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,1 +1,8 @@
+"""
+    __init__.py
+    创建蓝本程序：灵活定义路由
+"""
+
+from . import views, errors
 from flask import Blueprint
+main = Blueprint('main', __name__)
