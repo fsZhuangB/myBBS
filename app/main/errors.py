97557e57b020f102cdb3a0342acc3041044ae226
fszhuangb <617670307@qq.com>
2019-10-14 21:29:46 +0800

    add errors.py, forms.py, view.py

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,3 +2,15 @@
     errors.py
     错误处理程序
 """
+from . import main
+from flask import render_template
+
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
---
+++ app/main/forms.py
@@ -0,0 +1,16 @@
+"""
+    froms.py
+    表单对象
+"""
+from flask_wtf import Form
+from wtforms import StringField, SubmitField,validators
+
+class NameForm(Form):
+    """
+    这个函数用来生成表单
+    DataRequired()用来验证，保证提交的字段不为空
+    StringField类表示属性为type="text"的<input>元素
+    SubmitField类表示属性为type="submit"的<input>元素
+    """
+    name = StringField('What is your name?', [validators.DataRequired()])
+    submit = SubmitField('Submit')
